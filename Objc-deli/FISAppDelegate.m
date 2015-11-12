a5ecaeece28be90c8d6b28e049d81e89f444294d
hkxnyc <hannah.kim@flatironschool.com>
2015-11-11 19:48:23 -0500

    Deli

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -6,6 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
     // Override point for customization after application launch.
     return YES;
 }
@@ -15,5 +16,36 @@
  *  Define your methods here!
  
  */
+
+- (NSString*)stringWithDeliLine:(NSMutableArray *)deliLine {
+    NSMutableString *mDeliString = [NSMutableString new];
+    
+    if (deliLine.count == 0) {
+        [mDeliString appendString:@"The line is currently empty."];
+        return mDeliString;
+    }
+    
+    [mDeliString appendString:@"The line is:"];
+    for (NSUInteger i=0; i< deliLine.count; i++) {
+        NSUInteger numberInLine = i+1;
+        [mDeliString appendFormat:@"\n%lu. %@", numberInLine, deliLine[i]];
+    }
+    return [NSString stringWithString:mDeliString];
+}
+
+- (NSMutableArray*)addName:(NSString*)name toDeliLine:(NSMutableArray*)deliLine {
+    [deliLine addObject:name];
+    return deliLine;
+}
+
+- (NSString*)serveNextCustomerInDeliLine:(NSMutableArray*)deliLine {
+    NSString *nextCustomer = [deliLine firstObject];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+}
+
+
+
+
 
 @end
